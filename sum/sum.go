4a5d991edb43d7f3bd7a791e4bcb6cd15cca9131
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-01-16 21:33:59 -0300

    feat: SumAll function

--- sum/sum.go
+++ sum/sum.go
@@ -9,3 +9,13 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sum []int
+
+	for _, numbers := range numbersToSum {
+		sum = append(sum, Sum(numbers))
+	}
+
+	return sum
+}
