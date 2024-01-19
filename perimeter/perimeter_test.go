ca1fb2337356ad105cd3d3d46769bf47dac5e0b2
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-01-18 22:00:57 -0300

    feat: created structs for circle and rectangle

--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -2,22 +2,31 @@ package perimeter
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
-}
+// func TestPerimeter(t *testing.T) {
+// 	rectangle := Rectangle{10.0, 10.0}
+// 	got := Perimeter(rectangle)
+// 	want := 40.0
+//
+// 	if got != want {
+// 		t.Errorf("got %.2f want %.2f", got, want)
+// 	}
+// }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	want := 100.0
-
-	if got != want {
-		t.Errorf("want %.2f but got %.2f", want, got)
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
+
+	t.Run("rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkArea(t, rectangle, 100.0)
+	})
+	t.Run("circle area", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
 }
