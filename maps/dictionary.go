3d74e3cd70d2c33a480be9d6b95ce77182a15e95
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-01-21 18:20:17 -0300

    feat: implemented Search function with tests

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,17 @@
 package maps
 
-func Search(dictionary map[string]string, key string) string {
-	return "test"
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFoundWord = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(key string) (string, error) {
+	definition, ok := d[key]
+
+	if !ok {
+		return "", ErrNotFoundWord
+	}
+
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,11 +3,29 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{
+	dictionary := Dictionary{
 		"test": "this is a dictionary",
 	}
-	got := Search(dictionary, "test")
-	want := "this is a dictionary"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is a dictionary"
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := ErrNotFoundWord
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertStrings(t, err.Error(), want.Error())
+	})
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q, given %q", got, want, "test")
