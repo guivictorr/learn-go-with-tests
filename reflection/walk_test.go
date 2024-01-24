e94dbb99f370a3773c08c23c7802dc0b6938bf67
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-01-24 20:38:58 -0300

    feat: finishing reflection

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -58,7 +58,15 @@ func TestWalk(t *testing.T) {
 			"slices",
 			[]Profile{
 				{"London", 33},
-				{"Reykjavík", 33},
+				{"Reykjavík", 34},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+		{
+			"arrays",
+			[2]Profile{
+				{"London", 33},
+				{"Reykjavík", 34},
 			},
 			[]string{"London", "Reykjavík"},
 		},
@@ -75,5 +83,72 @@ func TestWalk(t *testing.T) {
 				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
 			}
 		})
+	}
+
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Cow":   "Moo",
+			"Sheep": "Baa",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Moo")
+		assertContains(t, got, "Baa")
+	})
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{"Berlin", 33}
+			aChannel <- Profile{"Katowice", 34}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{"Berlin", 33}, Profile{"Katowice", 34}
+		}
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
+
+func assertContains(t testing.TB, haystack []string, needle string) {
+	t.Helper()
+	contains := false
+
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+
+	if !contains {
+		t.Errorf("expected %v to contain %q but didn't", haystack, needle)
 	}
 }
